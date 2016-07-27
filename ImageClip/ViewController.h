f5bb89cc1fa5449294db2e7c9dde4c3e516b5c4f
KnightSama <eastzqh@gmail.com>
2016-07-27 15:26:28 +0800

    Initial Commit

---
+++ ImageClip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageClip
+//
+//  Created by KnightSama on 16/7/27.
+//  Copyright © 2016年 KnightSama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
